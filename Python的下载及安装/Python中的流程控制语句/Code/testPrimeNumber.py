0e80f81e934a6a24933f3ad591a3e62c8eaaa1e8
XiaojieWang <xiaojie_wangxj@163.com>
2019-04-02 23:13:07 +0800

    Delete testPrimeNumber.py

--- Python的下载及安装/Python中的流程控制语句/Code/testPrimeNumber.py
+++
@@ -1,9 +0,0 @@
-# 质数定义为在大于1的自然数中，除了1和它本身以外不再有其他因数。
-for i in range(2,10):
-    for j in range(2,i):
-        if i%j == 0:
-            print(i,"=",j,"*",i//j)
-            continue
-    else:
-        print(i,"is a prime number")
-        
